1d8524f073f7aff4e0c484c24369683a7a3e9304
GuilhermeMack <10738373@mackenzista.com.br>
2025-08-12 21:58:17 -0300

    Create exemplo1.c

---
+++ exemplo1.c
@@ -0,0 +1,18 @@
+/*
+ * Lab. 1 
+ * Exemplo 1
+ */
+ 
+#include <stdio.h>
+
+int main() {
+	// imprime algumas mensagens na tela
+	printf("Mackenzie - FCI\n");
+	printf("Algoritmos e ");
+	printf("Prog. II\n");
+	printf(Turma: 02P)
+	
+	// pausa antes de terminar o programa
+	system("pause");	
+	return 0;
+}
